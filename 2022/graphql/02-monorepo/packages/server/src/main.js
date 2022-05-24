8ddf104631cb9c77a4007715d9d1cbc0a08d01c8
pedro-drosa <pedro_drosa@hotmail.com>
2022-05-24 08:02:27 -0300

    chore: add express

--- 2022/graphql/02-monorepo/packages/server/src/main.js
+++ 2022/graphql/02-monorepo/packages/server/src/main.js
@@ -1,41 +1,19 @@
-import { createServer } from "http";
-import { parse } from "querystring";
+import express from "express";
 
-const server = createServer((req, res) => {
-  switch (req.url) {
-    case "/status": {
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(
-        JSON.stringify({
-          message: "Hello World!",
-        })
-      );
-      break;
-    }
-    case "/authenticate": {
-      let data = "";
-      req.on("data", (chunck) => {
-        data += chunck;
-      });
+const server = express();
 
-      req.on("end", () => {
-        const params = parse(data); // dados recebidos no form
-        res.end();
-      });
-      break;
-    }
-
-    default: {
-      res.writeHead(404, { "Content-Type": "application/json" });
-      res.end(
-        JSON.stringify({
-          error: "Page not found!",
-        })
-      );
-      break;
-    }
-  }
+server.get("/", (req, res) => {
+  res.send({ status: "Ok" });
 });
+
+server.post("/signin", express.json(), (req, res) => {
+  console.log({
+    email: req.body.email,
+    password: req.body.password,
+  });
+  res.send();
+});
+
 const APP_PORT = process.env.PORT | 5000;
 const BASE_URL = "http://localhost";
 
