1cf5fbaca1cfc22547c9ccb21b8b853c0a494c68
pedro-drosa <pedro_drosa@hotmail.com>
2022-05-24 10:33:01 -0300

    feat: add graphql and apolo-server-express

--- 2022/graphql/02-monorepo/packages/server/src/main.js
+++ 2022/graphql/02-monorepo/packages/server/src/main.js
@@ -1,28 +1,69 @@
 import express from "express";
-import cors from "cors";
-const server = express();
+import { ApolloServer, gql } from "apollo-server-express";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+// import cors from "cors";
 
-const enableCors = cors({
-  origin: "http://localhost:3000",
-});
+const app = express();
 
-server.get("/status", (req, res) => {
-  res.send({ status: "Ok" });
-});
+async function startServer() {
+  const server = new ApolloServer({
+    plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+    typeDefs: gql`
+      type Client {
+        id: ID!
+        name: String!
+      }
 
-server
-  .options("/authenticate", enableCors)
-  .post("/authenticate", enableCors, express.json(), (req, res) => {
-    console.log({
-      email: req.body.email,
-      password: req.body.password,
-    });
-    res.send({ status: true });
+      type Demand {
+        id: String!
+        name: String
+        client: Client
+        deadline: String
+      }
+
+      type Query {
+        demands: [Demand]!
+      }
+    `,
+
+    resolvers: {
+      Query: {
+        demands: () => [],
+      },
+    },
   });
+
+  await server.start();
+
+  server.applyMiddleware({
+    app,
+    cors: { origin: "http://localhost:3000" },
+  });
+}
+
+startServer(app);
+
+// const enableCors = cors({
+//   origin: "http://localhost:3000",
+// });
+
+// server.get("/status", (req, res) => {
+//   res.send({ status: "Ok" });
+// });
+
+// server
+//   .options("/authenticate", enableCors)
+//   .post("/authenticate", enableCors, express.json(), (req, res) => {
+//     console.log({
+//       email: req.body.email,
+//       password: req.body.password,
+//     });
+//     res.send({ status: true });
+//   });
 
 const APP_PORT = process.env.PORT | 5000;
 const BASE_URL = "http://localhost";
 
-server.listen(5000, () => {
+app.listen(5000, () => {
   console.log(`server is running on ${BASE_URL}:${APP_PORT}`);
 });
