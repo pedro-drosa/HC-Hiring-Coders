53f6ada984565603243ee296514bc1028226dc14
pedro-drosa <pedro_drosa@hotmail.com>
2022-06-01 09:41:23 -0300

    chore: add eslint and prettier

--- 2022/node-js/04-projeto-02/src/App.ts
+++ 2022/node-js/04-projeto-02/src/App.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express from 'express';
 
 class App {
   public server;
@@ -8,6 +8,7 @@ class App {
   }
 
   routes() {}
+
   middlewares() {}
 }
 
--- 2022/node-js/04-projeto-02/src/server.ts
+++ 2022/node-js/04-projeto-02/src/server.ts
@@ -1,6 +1,6 @@
-import App from "./App";
+import App from './App';
 
-const url = "http://localhost";
+const url = 'http://localhost';
 const port = 5000;
 
 App.listen(port, () => {
