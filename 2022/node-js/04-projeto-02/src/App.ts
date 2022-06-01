8fd58beaae58cdcf7c30e9c5dfde0e8141861670
pedro-drosa <pedro_drosa@hotmail.com>
2022-06-01 08:53:23 -0300

    chore: initial setup

---
+++ 2022/node-js/04-projeto-02/src/App.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+class App {
+  public server;
+
+  constructor() {
+    this.server = express();
+  }
+
+  routes() {}
+  middlewares() {}
+}
+
+export default new App().server;
---
+++ 2022/node-js/04-projeto-02/src/server.ts
@@ -0,0 +1,8 @@
+import App from "./App";
+
+const url = "http://localhost";
+const port = 5000;
+
+App.listen(port, () => {
+  console.log(`server running on: ${url}:${port}`);
+});
