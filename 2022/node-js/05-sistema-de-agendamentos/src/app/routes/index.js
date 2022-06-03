02b15378b7a2c187d0c3aa60baa2195cd2183464
pedro-drosa <pedro_drosa@hotmail.com>
2022-06-03 19:36:17 -0300

    feat: prevent the user from registering the same emails

--- 2022/node-js/05-sistema-de-agendamentos/src/app/routes/index.js
+++ 2022/node-js/05-sistema-de-agendamentos/src/app/routes/index.js
@@ -1,9 +1,8 @@
 import { Router } from "express";
+import UserController from "../controllers/UserController";
 
 const routes = Router();
 
-routes.get("/", (req, res) => {
-  res.send("Hello World");
-});
+routes.post("/users", UserController.store);
 
 export default routes;
