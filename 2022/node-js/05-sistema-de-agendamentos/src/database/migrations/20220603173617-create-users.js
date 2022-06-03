a59e39acadcc56df632f79c1bdcc6de88d8f513a
pedro-drosa <pedro_drosa@hotmail.com>
2022-06-03 14:55:22 -0300

    feat: add migration create-users

---
+++ 2022/node-js/05-sistema-de-agendamentos/src/database/migrations/20220603173617-create-users.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const { password } = require("pg/lib/defaults");
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("users", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true,
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      provider: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("users");
+  },
+};
