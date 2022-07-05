12cdc68b2d6e456c66be378909512ec374f8098d
pedro-drosa <pedro_drosa@hotmail.com>
2022-07-05 15:06:31 -0300

    feat: set configurable styles

--- 2022/vtex-io/store-block-template/react/Countdown.tsx
+++ 2022/vtex-io/store-block-template/react/Countdown.tsx
@@ -1,16 +1,19 @@
 import React, { useState } from "react";
 import { TimeSplit } from "./typings/global";
 import { tick, getTwoDaysFromNow } from "./utils/time";
+import { useCssHandles } from "vtex.css-handles";
 
 interface CountdownProps {
   targetDate: string;
 }
 
 const DEFAULT_TARGET_DATE = getTwoDaysFromNow();
+const CSS_HANDLES = ["countdown"];
 
 const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
   targetDate = DEFAULT_TARGET_DATE,
 }) => {
+  const handles = useCssHandles(CSS_HANDLES);
   const [timeRemaining, setTime] = useState<TimeSplit>({
     hours: "00",
     minutes: "00",
@@ -20,7 +23,7 @@ const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
   tick(targetDate, setTime);
 
   return (
-    <div>
+    <div className={`${handles.countdown} c-muted-1 db tc`}>
       <h1>{`${timeRemaining.hours}-${timeRemaining.minutes}-${timeRemaining.seconds}`}</h1>
     </div>
   );
