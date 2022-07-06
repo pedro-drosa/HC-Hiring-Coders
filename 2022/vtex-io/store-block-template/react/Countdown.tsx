3792b26fb36727a2ff168551c6c9c44048cd1db4
pedro-drosa <pedro_drosa@hotmail.com>
2022-07-06 09:57:58 -0300

    feat(useProduct): connecting backend and frontend

--- 2022/vtex-io/store-block-template/react/Countdown.tsx
+++ 2022/vtex-io/store-block-template/react/Countdown.tsx
@@ -1,18 +1,19 @@
 import React, { useState } from "react";
+import { useQuery } from "react-apollo";
+import { useProduct } from "vtex.product-context";
+import { useCssHandles } from "vtex.css-handles";
 import { TimeSplit } from "./typings/global";
 import { tick, getTwoDaysFromNow } from "./utils/time";
-import { useCssHandles } from "vtex.css-handles";
+import productReleaseDate from "./graphql/productReleaseDate.graphql";
 
 interface CountdownProps {
-  targetDate: string;
+  targetDate?: string;
 }
 
 const DEFAULT_TARGET_DATE = getTwoDaysFromNow();
 const CSS_HANDLES = ["countdown"];
 
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
-  targetDate = DEFAULT_TARGET_DATE,
-}) => {
+const Countdown: StorefrontFunctionComponent<CountdownProps> = ({}) => {
   const handles = useCssHandles(CSS_HANDLES);
   const [timeRemaining, setTime] = useState<TimeSplit>({
     hours: "00",
@@ -20,7 +21,38 @@ const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
     seconds: "00",
   });
 
-  tick(targetDate, setTime);
+  const { product } = useProduct();
+  const { data, loading, error } = useQuery(productReleaseDate, {
+    variables: {
+      slug: product?.linkText,
+    },
+    ssr: false,
+  });
+
+  if (!product) {
+    return (
+      <div>
+        <span>There is no product context.</span>
+      </div>
+    );
+  }
+
+  if (loading) {
+    return (
+      <div>
+        <span>Loading...</span>
+      </div>
+    );
+  }
+  if (error) {
+    return (
+      <div>
+        <span>Error!</span>
+      </div>
+    );
+  }
+
+  tick(data?.product?.releaseDate || DEFAULT_TARGET_DATE, setTime);
 
   return (
     <div className={`${handles.countdown} db tc c-muted-1`}>
