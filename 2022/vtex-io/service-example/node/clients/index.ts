6492b23ccd9c1d8b30b149a7dff30a86d18fd2e1
pedro-drosa <pedro_drosa@hotmail.com>
2022-07-11 11:29:13 -0300

    feat: use the customer catalog to make requests

--- 2022/vtex-io/service-example/node/clients/index.ts
+++ 2022/vtex-io/service-example/node/clients/index.ts
@@ -1,4 +1,5 @@
 import { IOClients } from '@vtex/api'
+import { Catalog } from '@vtex/clients'
 
 import Status from './status'
 
@@ -6,5 +7,9 @@ import Status from './status'
 export class Clients extends IOClients {
   public get status() {
     return this.getOrSet('status', Status)
+  }
+
+  public get catalog() {
+    return this.getOrSet('catalog', Catalog)
   }
 }
--- 2022/vtex-io/service-example/node/middlewares/status.ts
+++ 2022/vtex-io/service-example/node/middlewares/status.ts
@@ -1,27 +1,13 @@
 export async function status(ctx: Context, next: () => Promise<any>) {
   const {
     state: { code },
-    clients: { status: statusClient },
+    clients: { catalog },
   } = ctx
 
-  console.info('Received code:', code)
 
-  const statusResponse = await statusClient.getStatus(code)
-
-  console.info('Status response:', statusResponse)
-
-  const {
-    headers,
-    data,
-    status: responseStatus,
-  } = await statusClient.getStatusWithHeaders(code)
-
-  console.info('Status headers', headers)
-  console.info('Status data:', data)
-
-  ctx.status = responseStatus
+  const data  = await catalog.getSkuById(code.toString())
   ctx.body = data
-  ctx.set('Cache-Control', headers['cache-control'])
+  ctx.set('Cache-Control', 'no-cache no-store')
 
   await next()
 }
