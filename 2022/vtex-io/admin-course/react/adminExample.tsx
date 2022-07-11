88013f95cbb6cea9f0032117e31a7e8270622542
pedro-drosa <pedro_drosa@hotmail.com>
2022-07-11 10:44:34 -0300

    feat: use graphql query on the application front

--- 2022/vtex-io/admin-course/react/adminExample.tsx
+++ 2022/vtex-io/admin-course/react/adminExample.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Layout, PageBlock } from 'vtex.styleguide'
+import { useQuery } from 'react-apollo'
+
+import helloworld from './graphql/helloworld.gql'
 
 const AdminExample: React.FC = () => {
+  const { data } = useQuery(helloworld)
+
   return (
     <Layout>
       <PageBlock title="Titulo" subtitle="Alguma explicação." variation="full">
         <h1>Hello, World!</h1>
+        <p>{data?.helloworld}</p>
       </PageBlock>
     </Layout>
   )
